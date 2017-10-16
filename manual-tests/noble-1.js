dab1ad5adaf4b731357e3c7fd547b8f387b9fb8b
Iván Sánchez Ortega <ivan@sanchezortega.es>
2017-10-16 16:35:28 +0200

    Added noble example

---
+++ manual-tests/noble-1.js
@@ -0,0 +1,98 @@
+var noble = require('noble');
+let nrfDfu = require('../dist/nrf-dfu.cjs');
+
+// noble.startScanning(); // any service UUID, no duplicates 
+
+noble.on('stateChange', (state)=>{
+    console.log(state);
+  
+    if (state === 'poweredOn') {
+        noble.startScanning();
+    } else {
+        noble.stopScanning();
+    }
+});
+
+const updatesPromise = nrfDfu.DfuUpdates.fromZipFilePath('./spec/test-data/ble_app_buttonless_dfu_with_bonds_s132.zip')
+
+
+noble.on('discover', function(peripheral) {
+    console.log('Found device with local name: ' + peripheral.advertisement.localName);
+    console.log('address', peripheral.addressType, peripheral.address);
+    console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids);
+    console.log('rssi', peripheral.rssi);
+//     console.log(peripheral);
+//     console.log('id', peripheral.id);
+//     console.log('uuid', peripheral.id);
+//     console.log('advertisement', peripheral.advertisement);
+//     console.log('connectable', peripheral.connectable);
+//     console.log('services', peripheral.services);
+    console.log();
+    
+    // If the device is advertising a Nordic Secure DFU (0xfe59) service...
+//     if (peripheral.advertisement.serviceUuids.indexOf('fe59') !== 1){
+    
+    // If the device is advertiing a "DfuTest" name...
+    if (peripheral.advertisement.localName === 'DfuTest'){
+        noble.stopScanning();   // Stop scanning for more devices
+        
+        let nobleTransport = new nrfDfu.DfuTransportNoble(peripheral, 2);
+        
+//         nobleTransport._ready().then(()=>{
+//             nobleTransport._writeCommand(new Uint8Array([0x06, 0x02]));
+//             
+//             nobleTransport._read().then((bytes)=>{
+//                 console.log('Received: ', bytes);
+//             });
+//         });
+        
+        
+        
+        updatesPromise.then((updates)=>{
+            let dfu = new nrfDfu.DfuOperation(updates, nobleTransport);
+            dfu.start(true)
+            .then(()=>{
+                console.log('Seems like the DFU completed successfully!!')
+            })
+            .catch((err)=>{
+                console.error('DFU failed. Reason:');
+                console.error(err);
+            })
+            .then(()=>{
+//                 port.close()
+            });
+        });
+        
+//         console.log('Device has a Nordic DFU BLE service; connecting...');
+//         peripheral.connect(function(error) {
+//             console.log('connected to peripheral: ' + peripheral.uuid);
+//             peripheral.discoverServices(['fe59'], function(error, services) {
+//                 var deviceInformationService = services[0];
+//                 console.log('discovered device information service');
+// 
+//                 deviceInformationService.discoverCharacteristics(null, function(error, characteristics) {
+//                     console.log('discovered the following characteristics:');
+//                     for (var i in characteristics) {
+//                         console.log('  ' + i + ' uuid: ' + characteristics[i].uuid);
+//                     }
+//                 });
+//             });
+//         });
+    }
+    
+});
+
+
+
+
+
+
+
+
+// noble.startScanning([], true); // any service UUID, allow duplicates 
+  
+// var serviceUUIDs = ["<service UUID 1>", ...]; // default: [] => all 
+// var allowDuplicates = <false|true>; // default: false 
+ 
+// noble.startScanning(serviceUUIDs, allowDuplicates[, callback(error)]); // particular UUID's 
+
